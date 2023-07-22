6e0c02fbb8c2341870a686a327d142e98ce4f7a5
TylerC53 <123029553+TylerC53@users.noreply.github.com>
2023-07-22 19:55:17 -0400

    Add files via upload

---
+++ controllers/roomController.js
@@ -0,0 +1,45 @@
+const Meeting = require('../models/RoomModel') // Import room model
+                                                  // so we can work with DB
+                                                  // using Model
+const mongoose = require('mongoose')
+
+// Admin Only Functions:
+// Create a single room
+const createRoom = async (req, res) => {
+    const {number, specialroom} = req.body
+
+    try {
+        const room = await Room.create({number, specialroom})
+        res.status(200).json(room)
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
+}
+
+// Delete a single room
+const deleteRoom = async (req, res) => {
+    const{ number } = req.params
+
+    if(!mongoose.Types.ObjectID.isValid(number)){
+        return res.status(404).json({error: 'No room found'})
+    }
+
+    const room = await Room.findOneAndDelete({_number: number})
+
+    if(!room){
+        return res.status(400).json({error: 'No room found'})
+    }
+
+    res.status(200).json(room)
+}
+
+// Admin and Client Functions:
+// Reserve a single room (Also needs to charge if room is special)
+const reserveRoom = async (req, res) => {
+
+}
+module.exports = {
+    createRoom,
+    deleteRoom,
+    reserveRoom
+}
