6b472274cb602a5327784cc3f9dd0e1957f5c7b7
TylerC53 <123029553+TylerC53@users.noreply.github.com>
2023-07-22 19:36:52 -0400

    Add files via upload

---
+++ controllers/complaintController.js
@@ -0,0 +1,48 @@
+const Complaint = require('../models/ComplaintModel')   // Import complaint model
+                                                        // so we can work with DB
+                                                        // using Model
+const mongoose = require('mongoose')
+
+// Admin only functions:
+// Get all complaints
+const getAllComplaints = async (req, res) => {
+    const complaints = await Complaint.find({}).sort({createdAt: -1})
+
+    res.status(200).json(complaints)
+}
+
+// Respond to a single complaint
+const updateComplaint = async (req, res) => {
+    const{ id } = req.params
+
+    if(!mongoose.Types.ObjectID.isValid(id)){
+        return res.status(404).json({error: 'No complaint found'})
+    }
+
+    const complaint = await Complaint.findOneAndUpdate({_id: id}, {...req.body})
+
+    if(!complaint){
+        return res.status(400).json({error: 'No complaint found'})
+    }
+
+    res.status(200).json(complaint)
+}
+
+// Admin and Client functions:
+// Create a single complaint
+const createComplaint = async (req, res) => {
+    const {id, subject, email, message} = req.body
+
+    try {
+        const complaint = await Complaint.create({id, subject, email, message})
+        res.status(200).json(complaint)
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
+}
+
+module.exports = {
+    getAllComplaints,
+    updateComplaint,
+    createComplaint
+}
