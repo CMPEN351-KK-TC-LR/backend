70c585a69fb056d361d65f6f535a52c755074b52
kkeian <kaserman22@gmail.com>
2023-08-11 01:34:21 -0400

    Create too short tests for register user

--- tests/user.test.js
+++ tests/user.test.js
@@ -17,7 +17,43 @@ describe('user tests', () => {
         agent
             .post("/api/users/register")
             .expect(400, done)
-        });
+        })
+    
+    it('sends 400 error with email too short', (done) => {
+        const user = {
+            name: "Keian Kaserman",
+            email: "ks@pennstatesoft.com",
+            password: "password123"
+        }
+        agent
+            .post('/api/users/register')
+            .send(user)
+            .expect(400, done)
+    })
+
+    it('sends 400 error with name too short', (done) => {
+        const user = {
+            name: "ds",
+            email: "k.s@pennstatesoft.com",
+            password: "password123"
+        }
+        agent
+            .post('/api/users/register')
+            .send(user)
+            .expect(400, done)
+    })
+
+    it('sends 400 error with password too short', (done) => {
+        const user = {
+            name: "Keian Kaserman",
+            email: "k.s@pennstatesoft.com",
+            password: "password123"
+        }
+        agent
+            .post('/api/users/register')
+            .send(user)
+            .expect(400, done)
+    })
 })
     
   
